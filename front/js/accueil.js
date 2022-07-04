55fa0f6b5a52eed62e76cda2b52039e321119fa7
Ben <46194536+Ahebi@users.noreply.github.com>
2022-07-04 12:15:45 +0200

    Create accueil.js
    dsd

---
+++ front/js/accueil.js
@@ -0,0 +1,16 @@
+fetch("http://localhost:3000/api/products")
+
+.then(function(res) {
+    if (res.ok) {
+        return res.json(); 
+      }
+})
+
+.then (function(value) {
+
+    console.log(value);
+})
+
+.catch(function(err) {
+    // une erreur est survenue
+});
