fa0d2fb487009304ea1d2cfd9f4943835bdfae8d
Ben <46194536+Ahebi@users.noreply.github.com>
2022-07-06 14:35:19 +0200

    innerhtml

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,15 @@
 console.log("test console")
 
 function api() {
+
+    let htmlproduit =   '<a href="./product.html?id=42"> <article><img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1"><h3 class="productName">Kanap name1</h3><p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p> </article>  </a>';
+   
+        
+                      
+
+
     console.log("Je suis dans la fonction api()");
+   
     fetch("http://localhost:3000/api/products")
         .then(function(res) {
             if (res.ok) {
@@ -17,13 +25,14 @@ function api() {
             for(let i=0;i<products.length;i++) {
                 console.log("Je prends le produit " + i);
                 console.log(products[i].name);
-                items.innerHTML += products[i].name;
+                
+                items.innerHTML = htmlproduit;
             }
         })
         .catch(function(_err) {
             console.log("Erreur !");
             console.log(_err);
-        });
+        });  
 }
 
 console.log("J'appele la fonction api()")
