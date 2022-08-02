2028c7a68648449daa02a2ed4f1ea73f66e16fc2
Ben <46194536+Ahebi@users.noreply.github.com>
2022-08-02 16:29:59 +0200

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,77 +1,81 @@
 function affichagePanier() {
-    
-        
-            
+  const interfacePanier = document.getElementById("cart__items");
+  let panier = JSON.parse(localStorage.getItem("panier"));
+  console.log("panier.lenght = "+ panier.length);
+  const divPrixTotal = document.getElementById("totalPrice")
+  
+  fetch("http://localhost:3000/api/products/")
+    .then(function(res) {
+      if (res.ok) {
+          return res.json(); 
+      }
+    })
+    .then (function(products) {
+      let prixTotal = 0;
+      for (let i in panier) {
+        let id = panier[i]["id"];
+        let color = panier[i]["couleur"];
+        let quantite = panier[i]["quantite"];
+       
+        let product = products.find(function (p) {
+          return p._id === id;
+        });
+      
+        console.log("Le produit trouvé :")
+        console.log(product);
 
-            const interfacePanier = document.getElementById("cart__items");
-            let panier = JSON.parse(localStorage.getItem("panier"));
-            console.log("panier.lenght = "+ panier.length);
-            const divPrixTotal = document.getElementById("totalPrice")
-           
-            
-            
-           for (let i in panier){
-            
-            let id = panier[i]["id"];
-            let color = panier[i]["couleur"];
-            let quantite = panier[i]["quantite"];
+        let prixLigne = product.price * quantite;
+        prixTotal = prixTotal + prixLigne;
+        console.log("Prix total : " + prixTotal);
 
-            fetch("http://localhost:3000/api/products/"+id )
-        .then(function(res) {
-            if (res.ok) {
-                return res.json(); 
-            }
-        })
-      .then (function(product) {
-            console.log("Le produit reçu :")
-            console.log(product);
-
-            interfacePanier.innerHTML += ` <article class="cart__item" data-id="${id}" data-color="${color}">
-            <div class="cart__item__img">
-              <img src="${product.imageUrl}" alt="${product.altTxt}">
+        interfacePanier.innerHTML += ` <article class="cart__item" data-id="${id}" data-color="${color}">
+          <div class="cart__item__img">
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+          </div>
+          <div class="cart__item__content">
+            <div class="cart__item__content__description">
+              <h2>${product.name}</h2>
+              <p>${color}</p>
+              <p>${product.price}€</p>
             </div>
-            <div class="cart__item__content">
-              <div class="cart__item__content__description">
-                <h2>${product.name}</h2>
-                <p>${color}</p>
-                <p>${product.price}€</p>
+            <div class="cart__item__content__settings">
+              <div class="cart__item__content__settings__quantity">
+                <p>Qté :</p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantite}">
               </div>
-              <div class="cart__item__content__settings">
-                <div class="cart__item__content__settings__quantity">
-                  <p>Qté :</p>
-                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantite}">
-                </div>
-                <div class="cart__item__content__settings__delete">
-                  <p class="deleteItem">Supprimer</p>
-                </div>
+              <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
               </div>
             </div>
-          </article>`
-         
+          </div>
+        </article>`;
+      }
+      // Tout les produits ont bien été ajouté
+      divPrixTotal.innerHTML = prixTotal;
+
+      // Ajout des listener pour le changement de quantité
+      const quantityItemCart = document.getElementsByClassName("itemQuantity");
+
+      for(let i = 0;i<quantityItemCart.length;i++) {
+        quantityItemCart[i].addEventListener('change', function (event){
+          let newQuantite = event.target.value;
+
+          let article = event.target.closest("article");
+          let id = article.getAttribute("data-id");
+          let color = article.getAttribute("data-color");
           
+          //  Changer dans le localstorage la quantité
+          console.log("Nouvelle quantité : " + newQuantite);
+
+          // .... Puis : recalculer le total et la quantité totale du panier
+        })
+      }
 
     })
     .catch(function(_err) {
-            console.log("Erreur !");
-            console.log(_err);
-        });  
-
-            
-           }
- 
-          let productPrice = product.price
-          let prixTotal = product.price * quantite
-          divPrixTotal.innerHTML = prixTotal;
-       
+      console.log("Erreur !");
+      console.log(_err);
+    });  
 }
 
 affichagePanier();
-
-
-const quantityItemCart = document.getElementsByClassName("itemQuantity");
-
-quantityItemCart[0].addEventListener('change', function (){
-
-  let newQantite = document.getElementsByClassName("itemQuantity").value;
-  
-})
